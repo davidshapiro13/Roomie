cb29c3b20de512c967a7650ac7fa42a59aa78e91
davidshapiro13 <davidshapiro@brandeis.edu>
2024-08-28 16:44:24 -0400

    Initial Commit

---
+++ Roomie/RoomieApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomieApp.swift
+//  Roomie
+//
+//  Created by David Shapiro on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
